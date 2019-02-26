eeeb2eba49ee3fe10b250c8252a7ac1d41b5adba
fauzanakmalh1 <fauzanakmalhariz1@gmail.com>
2019-02-26 10:04:33 +0700

    Program Swap

--- Fauzan_Swap.cpp
+++ Fauzan_Swap.cpp
@@ -1,5 +1,5 @@
 /*
-Nama Program    : Swap
+Nama Program    : Program Swap
 Nama            : Fauzan Akmal Hariz
 NPM             : 140810180005
 Tanggal buat    : 26 Februari 2019
@@ -26,7 +26,7 @@ int main()              //program utama
     cout<<"x adalah : " <<x <<endl;
     cout<<"y adalah : " <<y <<endl <<endl;
 
-    swap(x,y);                  //memanggil fungsi swao
+    swap(x,y);                  //memanggil fungsi swap
 
     cout<<"Setelah di Swap" <<endl <<endl;
     cout<<"x adalah : " <<x <<endl;
