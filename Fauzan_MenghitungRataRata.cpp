0a282689867ac57df4fdb8d112e07c22e41b66ba
fauzanakmalh1 <fauzanakmalhariz1@gmail.com>
2019-02-26 10:13:54 +0700

    Program Menghitung Rata-Rata

---
+++ Fauzan_MenghitungRataRata.cpp
@@ -0,0 +1,20 @@
+/*
+Nama Program    : Program Menghitung Rata-Rata
+Nama            : Fauzan Akmal Hariz
+NPM             : 140810180005
+Tanggal buat    : 26 Februari 2019
+Deskripsi       : Exercise 02 Praktikum
+*/
+
+#include <iostream>
+
+using namespace std;
+
+float average();
+
+void input();
+
+int main()
+{
+
+}
